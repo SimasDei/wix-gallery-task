7e6adaa2234327f2e137910904ab5764e86846c7
SimasDei <simondeiman@gmail.com>
2019-02-02 12:37:37 +0200

    Use switch statment instead of ifelse, to provide a valid default module

--- main/javascript/ImageFinder.js
+++ main/javascript/ImageFinder.js
@@ -4,19 +4,45 @@
   // Turn into Asyncronous call in order to handle
   // Api fetch Promise
   ImageFinder.prototype.search = async (query, moduleId) => {
-    if (moduleId === 'flickr') {
-      return {
-        query,
-        images: await window.MODULES.flickr(query)
-      };
-    } else if (moduleId === 'static') {
-      return {
-        query: query,
-        images: window.MODULES.static(query)
-      };
-    } else {
-      let error = 'Unknown Search Module';
-      throw error;
+    // Use Switch instead to not get an unknown module error on page load
+    switch (moduleId) {
+      case 'static':
+        return {
+          query: query,
+          images: window.MODULES.static(query)
+        };
+        break;
+      case 'flickr':
+        return {
+          query,
+          images: await window.MODULES.flickr(query)
+        };
+
+      case !'static' || !'flickr':
+        const error = 'Unknown Module';
+        throw error;
+        break;
+      default:
+        return {
+          query: query,
+          images: window.MODULES.static(query)
+        };
+        break;
     }
+
+    // if (moduleId === 'flickr') {
+    //   return {
+    //     query,
+    //     images: await window.MODULES.flickr(query)
+    //   };
+    // } else if (moduleId === 'static') {
+    //   return {
+    //     query: query,
+    //     images: window.MODULES.static(query)
+    //   };
+    // } else {
+    //   let error = 'Unknown Search Module';
+    //   throw error;
+    // }
   };
 })();
--- main/javascript/static.js
+++ main/javascript/static.js
@@ -1,8 +1,9 @@
 window.MODULES.static = query => {
+  // Set static database
   let db = window.DATA.staticImagesDb;
-
+  // Filter the image array giving the query input
   let images = db.filter(image => image.title.includes(query));
-
+  // Refactor to the requested format
   let filteredImages = images.map(image => {
     return {
       id: image.id,
